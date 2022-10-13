25c51c9d361e1a8bfe59d4e8428df871e664789a
MeleeCreep <91787311+MeleeCreep@users.noreply.github.com>
2022-10-13 13:46:52 +0300

    Add files via upload
    Калькулятор

---
+++ WinForms_Calc_50114/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WinForms_Calc_50114
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_first = new System.Windows.Forms.TextBox();
+            this.textBox_second = new System.Windows.Forms.TextBox();
+            this.button_Sub = new System.Windows.Forms.Button();
+            this.label_Result = new System.Windows.Forms.Label();
+            this.button_DivAssig = new System.Windows.Forms.Button();
+            this.button_Or = new System.Windows.Forms.Button();
+            this.button_AndAssig = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_first
+            // 
+            this.textBox_first.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_first.Location = new System.Drawing.Point(12, 12);
+            this.textBox_first.Name = "textBox_first";
+            this.textBox_first.Size = new System.Drawing.Size(179, 34);
+            this.textBox_first.TabIndex = 0;
+            this.textBox_first.TextChanged += new System.EventHandler(this.textBox_first_TextChanged);
+            // 
+            // textBox_second
+            // 
+            this.textBox_second.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_second.Location = new System.Drawing.Point(12, 65);
+            this.textBox_second.Name = "textBox_second";
+            this.textBox_second.Size = new System.Drawing.Size(179, 34);
+            this.textBox_second.TabIndex = 1;
+            this.textBox_second.TextChanged += new System.EventHandler(this.textBox_second_TextChanged);
+            // 
+            // button_Sub
+            // 
+            this.button_Sub.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_Sub.Location = new System.Drawing.Point(198, 148);
+            this.button_Sub.Name = "button_Sub";
+            this.button_Sub.Size = new System.Drawing.Size(148, 46);
+            this.button_Sub.TabIndex = 2;
+            this.button_Sub.Text = "Вычитание";
+            this.button_Sub.UseVisualStyleBackColor = true;
+            this.button_Sub.Click += new System.EventHandler(this.button_Sub_Click);
+            // 
+            // label_Result
+            // 
+            this.label_Result.AutoSize = true;
+            this.label_Result.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Result.Location = new System.Drawing.Point(276, 65);
+            this.label_Result.Name = "label_Result";
+            this.label_Result.Size = new System.Drawing.Size(0, 26);
+            this.label_Result.TabIndex = 3;
+            // 
+            // button_DivAssig
+            // 
+            this.button_DivAssig.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_DivAssig.Location = new System.Drawing.Point(55, 236);
+            this.button_DivAssig.Name = "button_DivAssig";
+            this.button_DivAssig.Size = new System.Drawing.Size(291, 46);
+            this.button_DivAssig.TabIndex = 4;
+            this.button_DivAssig.Text = "Деление с присваиванием";
+            this.button_DivAssig.UseVisualStyleBackColor = true;
+            this.button_DivAssig.Click += new System.EventHandler(this.button_DivAssig_Click);
+            // 
+            // button_Or
+            // 
+            this.button_Or.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_Or.Location = new System.Drawing.Point(370, 148);
+            this.button_Or.Name = "button_Or";
+            this.button_Or.Size = new System.Drawing.Size(148, 46);
+            this.button_Or.TabIndex = 5;
+            this.button_Or.Text = "Или";
+            this.button_Or.UseVisualStyleBackColor = true;
+            this.button_Or.Click += new System.EventHandler(this.button_Or_Click);
+            // 
+            // button_AndAssig
+            // 
+            this.button_AndAssig.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_AndAssig.Location = new System.Drawing.Point(370, 236);
+            this.button_AndAssig.Name = "button_AndAssig";
+            this.button_AndAssig.Size = new System.Drawing.Size(291, 46);
+            this.button_AndAssig.TabIndex = 6;
+            this.button_AndAssig.Text = "И с присваиванием";
+            this.button_AndAssig.UseVisualStyleBackColor = true;
+            this.button_AndAssig.Click += new System.EventHandler(this.button_AndAssig_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(276, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_AndAssig);
+            this.Controls.Add(this.button_Or);
+            this.Controls.Add(this.button_DivAssig);
+            this.Controls.Add(this.label_Result);
+            this.Controls.Add(this.button_Sub);
+            this.Controls.Add(this.textBox_second);
+            this.Controls.Add(this.textBox_first);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_first;
+        private System.Windows.Forms.TextBox textBox_second;
+        private System.Windows.Forms.Button button_Sub;
+        private System.Windows.Forms.Label label_Result;
+        private System.Windows.Forms.Button button_DivAssig;
+        private System.Windows.Forms.Button button_Or;
+        private System.Windows.Forms.Button button_AndAssig;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForms_Calc_50114/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calc = ClassLib_Cacl_50114_1_.Calculator;
+
+namespace WinForms_Calc_50114
+{
+    public partial class Form1 : Form
+    {
+        private int _first_value = 0;
+        private int _second_value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_first_TextChanged(object sender, EventArgs e)
+        {
+            int value;
+            if (Int32.TryParse(textBox_first.Text, out value))
+            {// обновляем переменную класса, если парсинг выполнен 
+                _first_value = value;
+            }
+            else
+            {
+                _first_value = 0; // преобразование не было выполнено 
+            }
+        }
+        private void textBox_second_TextChanged(object sender, EventArgs e)
+        {
+            int value1;
+            if (Int32.TryParse(textBox_second.Text, out value1))
+            {// обновляем переменную класса, если парсинг выполнен 
+                _second_value = value1;
+            }
+            else
+            {
+                _second_value = 0; // преобразование не было выполнено 
+            }
+        }
+
+        private void button_Sub_Click(object sender, EventArgs e)
+        {
+            if (_second_value <= 80)
+            {
+                string str_message = Calc.Sub(_first_value, _second_value);
+                label_Result.Text = str_message;
+            }
+            else
+                MessageBox.Show("Второе число должно быть не более 80","Ошибка");
+        }
+
+        private void button_Or_Click(object sender, EventArgs e)
+        {
+            if (_first_value <= 90)
+            {
+                string str_message = Calc.Or((sbyte)_first_value, (sbyte)_second_value);
+                label_Result.Text = str_message;
+            }
+            else
+                MessageBox.Show("Первое число должно быть не более 90", "Ошибка");
+        }
+
+        private void button_DivAssig_Click(object sender, EventArgs e)
+        {
+            if (_second_value >= 30)
+            {
+                string str_message = Calc.DivAssig(_first_value, _second_value);
+                label_Result.Text = str_message;
+            }
+            else
+                MessageBox.Show("Второе число должно быть не менее 30", "Ошибка");
+        }
+
+        private void button_AndAssig_Click(object sender, EventArgs e)
+        {
+            if (_first_value >= 40)
+            {
+                string str_message = Calc.AndAssig(_first_value, _second_value);
+                label_Result.Text = str_message;
+                _first_value = Int32.Parse(str_message);
+                textBox_first.Text = str_message;
+            }
+            else
+                MessageBox.Show("Первое число должно быть не менее 40", "Ошибка");
+        }
+    }
+}
